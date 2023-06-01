42f6d1631994b623dfc6ee1f35437d755c80ce09
Ennyola <medunoyeeni@gmail.com>
2023-06-01 17:10:33 +0100

    more styling on the header and sidebar. Publish content page created

--- scheduler/views.py
+++ scheduler/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request, "scheduler/index.html")
+    return render(request, "scheduler/publish.html")
