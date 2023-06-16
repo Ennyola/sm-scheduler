3da2bb5e61100898e951ad4ef6a9c33b4c76dd8a
Ennyola <medunoyeeni@gmail.com>
2023-06-16 21:15:24 +0100

    added a formView in add accounts

--- social_media_integration/views.py
+++ social_media_integration/views.py
@@ -1,18 +1,19 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView,ListView
+from django.views.generic.edit import ProcessFormView
 
 from .models import SocialMediaAccount
 
 
-def manage_accounts(request):
-    social_accounts = SocialMediaAccount.objects.all()
-    context = {"social_accounts": social_accounts}
-    return render(request, "social_media_integration/manage_accounts.html", context)
-
+class ManageAccountsView(ListView):
+    queryset = SocialMediaAccount.objects.all()
+    context_object_name = "social_accounts"
+    template_name = "social_media_integration/manage_accounts.html"
 
 class AddAccountsView(TemplateView):
     template_name = "social_media_integration/add_social_accounts.html"
 
 
+manage_accounts = ManageAccountsView.as_view()
 add_accounts_templates_view = AddAccountsView.as_view()
