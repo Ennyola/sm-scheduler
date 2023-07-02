83206a72e8fcbe4412a012f374c2a208f36cca48
Ennyola <medunoyeeni@gmail.com>
2023-07-02 20:13:35 +0100

    changes

--- social_media_integration/views.py
+++ social_media_integration/views.py
@@ -1,9 +1,11 @@
 from django.shortcuts import render
 from django.views import View
-from django.views.generic import TemplateView,ListView
+from django.views.generic import TemplateView, ListView
 from django.views.generic.edit import ProcessFormView
+from django.shortcuts import redirect
 
 from .models import SocialMediaAccount
+from .tasks import add
 
 
 class ManageAccountsView(ListView):
@@ -11,14 +13,17 @@ class ManageAccountsView(ListView):
     context_object_name = "social_accounts"
     template_name = "social_media_integration/manage_accounts.html"
 
+
 class AddAccountsView(View):
     template_name = "social_media_integration/add_social_accounts.html"
-    
+
     def get(self, request, *args, **kwargs):
         return render(request, self.template_name)
-    
+
     def post(self, request, *args, **kwargs):
-        pass
+        if "twitter" in request.POST:
+            add.delay(20, 34)
+        return redirect("social_integration:add-social-accounts")
 
 
 manage_accounts = ManageAccountsView.as_view()
