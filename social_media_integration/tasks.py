b0a8325d3371b4a3d24052f8fc6895f21a0639c1
Ennyola <medunoyeeni@gmail.com>
2023-07-12 01:35:33 +0100

    get twitter authorization url added

--- social_media_integration/tasks.py
+++ social_media_integration/tasks.py
@@ -6,11 +6,12 @@ from celery import shared_task
 from requests_oauthlib import OAuth1Session
 
 from django.conf import settings
+from django.shortcuts import redirect
 
 USER = settings.AUTH_USER_MODEL
 consumer_key = os.environ.get("API_KEY")
 consumer_secret = os.environ.get("API_KEY_SECRET")
-
+oauth = None
 # Celery tasks
 
 
@@ -18,8 +19,14 @@ consumer_secret = os.environ.get("API_KEY_SECRET")
 def connect_twitter():
     request_token_url = "https://api.twitter.com/oauth/request_token"
     oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-    
-def connecticut():
+
+
+def get_twitter_authorization_url():
     request_token_url = "https://api.twitter.com/oauth/request_token"
     oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-    print(oauth)
+    oauth.fetch_request_token(request_token_url)
+    # Get authorization
+    base_authorization_url = "https://api.twitter.com/oauth/authorize"
+    authorization_url = oauth.authorization_url(base_authorization_url)
+    return authorization_url
+
--- social_media_integration/views.py
+++ social_media_integration/views.py
@@ -1,3 +1,5 @@
+from typing import Any
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.views import View
 from django.views.generic import TemplateView, ListView
@@ -5,24 +7,34 @@ from django.views.generic.edit import ProcessFormView
 from django.shortcuts import redirect
 
 from .models import SocialMediaAccount
-from .tasks import connect_twitter, connecticut
+from .tasks import connect_twitter, get_twitter_authorization_url
 
 
 class ManageAccountsView(ListView):
     queryset = SocialMediaAccount.objects.all()
     context_object_name = "social_accounts"
     template_name = "social_media_integration/manage_accounts.html"
-
-
+        
+    
 class AddAccountsView(View):
     template_name = "social_media_integration/add_social_accounts.html"
 
     def get(self, request, *args, **kwargs):
-        return render(request, self.template_name)
+        twitter_oauth_verifier = kwargs.get("oauth_verifier", None)
+        
+        # If no request has been made to any endpoint.
+        if twitter_oauth_verifier is None:
+            return render(request, self.template_name)
+        # Get the twitter oauth verifier
+        oauth_verifier = self.request.GET.get('oauth_verifier')
+        print(oauth_verifier)
+        
 
     def post(self, request, *args, **kwargs):
         if "twitter" in request.POST:
-           connecticut()
+           authorization_url = get_twitter_authorization_url()
+           print(authorization_url)
+           return redirect(authorization_url)
         return redirect("social_integration:add-social-accounts")
 
 
